672461a3c15ecae5f5556f6f8db4dbe6d0fef993
dak0rn <daniel@suitsoft.eu>
2015-04-21 10:20:20 +0200

    This could be what I wanted to have

--- randi.js
+++ randi.js
@@ -6,30 +6,41 @@
  */
 
 (function (root, factory) {
-        if (typeof define === 'function' && define.amd) {
-                    define([], factory);
-                        }
-            else if (typeof exports === 'object') {
-                        module.exports = factory();
-                            }
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    }
+    else if (typeof exports === 'object') {
+        module.exports = factory();
+    }
     else {
-                root.metacarattere = factory();
-                    }
+        root.metacarattere = factory();
+    }
 } (this, function () {
 
     return function(length, options) {
 
-        if( 'undefined' === options && 
+        if( 'undefined' === options &&
             null !== length &&
             'object' === typeof length ) {
             options = length;
             length  = undefined;
         }
 
+        options = options || {};
+        var charset = options.charset || "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
         var gen = function(length) {
+            var r = '';
+            var i = length + 1;
+            var l = charset.length;
+
+            while( --i > 0 )
+                r += charset[ Math.floor( Math.random() * l )  ];
+
+            return r;
         };
 
-        if( length && length >= 0 )
+        if( 'number' === typeof length && length >= 0 )
             return gen(length);
         else
             return gen;
